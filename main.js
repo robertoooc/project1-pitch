0e8cf37905c089b11411a843aff1096f69d54cf8
robertoooc <robertooo.c48@gmail.com>
2022-12-02 13:47:32 -0800

    having issues with set time interval, looking into using request animation and might need to restart certain parts.

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 //grabbing canvas
 const canvas = document.getElementById('canvas')
-//assigning canvas' resolution relative to the window
-canvas.setAttribute('height',getComputedStyle(canvas)['height'])
-canvas.setAttribute('width',getComputedStyle(canvas)['width'])
+let p = document.querySelector('p')
+
+let cWidth = parseInt(window.getComputedStyle(canvas)['width'])
+let cHeight = parseInt(window.getComputedStyle(canvas)['height'])
+
+let checkheight = false
+let checkwidth = false
+let check =false
+while(check==false){
+ cWidth % 10!==0 ? cWidth++ : checkwidth = true
+ cHeight % 10!==0 ? cHeight++ : checkheight = true
+ checkwidth&&checkheight ? check=true : check =false
+}
+canvas.setAttribute('height',cHeight.toString())
+canvas.setAttribute('width',cWidth.toString())
 
 //getting rendering context from the Canvas
 const ctx = canvas.getContext('2d')
@@ -24,29 +36,30 @@ class Objects{
     }
 }
 
-let leftWall = new Objects(0,0,1,canvas.height,"orange")
-let topWall = new Objects(0,0,canvas.width,1,"orange")
-let rightWall = new Objects(canvas.width -1,0,1,canvas.height,"orange")
-let bottomWall = new Objects(0,canvas.height-1,canvas.width,1,"orange")
+let leftWall = new Objects(0,0,5,canvas.height,"orange")
+let topWall = new Objects(0,0,canvas.width,5,"orange")
+let rightWall = new Objects(canvas.width -5,0,5,canvas.height,"orange")
+let bottomWall = new Objects(0,canvas.height-5,canvas.width,5,"orange")
 // let obstacle = new Objects(300,54,50,50,"black")
 // obstacle.create()
 // let line = new Objects()
 let access = true
-const downAccelerate = 0.5
+const downAccelerate = 5
 
 //made a class that will help out with player movement
 class Players{
     constructor(x,y,color){
         this.name
-        this.x = x
-        this.y = y
+        this.x = 0
+        this.y = 0
         this.access
         this.jump = {
-            up: 10,
-            gravity: 7
+            up: 20,
+            gravity: 0
         }
-        this.width = (canvas.height)/21
-        this.height = (canvas.width)/30
+        this.width = parseInt((canvas.height)/20)
+        this.height = 30//parseInt((canvas.width)/20)
+        console.log(this.height, this.width)
         this.color = color
     }
         create(){
@@ -54,21 +67,27 @@ class Players{
             ctx.fillRect(this.x,this.y,this.width,this.height)
         }
         gravityUpdate(){
+            p.innerText = `${this.x}, ${this.y}`
             this.create()
             this.y+=this.jump.gravity
-
-
-            if(access ==false){
+            if(this.y + this.height <= canvas.height-90){
+                this.jump.gravity+=downAccelerate
+                console.log(this.y + this.height, canvas.height)
+            }
+            else{
                 this.jump.gravity = 0
             }
-            this.jump.y+=downAccelerate
         }
+            shiftRight(){
+                this.x +=5
+                gravityUpdate()
+            }
             
         }
         
         let butcher = new Players(5,10,'red')
         butcher.create()
-        let piglet = new Players(40,90,'pink')
+        let piglet = new Players(60,10,'pink')
         piglet.create()
         
         
@@ -76,67 +95,65 @@ class Players{
         
         
         
-        const speed = 2;
+        //const speed = 5;
         function movement(){
-            if(pressedKeys.ArrowDown){
-                butcher.y += speed  
-            }
+            // if(pressedKeys.ArrowDown){
+            //     butcher.y += speed  
+            // }
             if(pressedKeys.ArrowUp){
-        butcher.y -= speed
-    }
-    if(pressedKeys.ArrowLeft){
-        butcher.x -=speed
-    }
-    if(pressedKeys.ArrowRight){
-        butcher.x +=speed
-    }
-    if( access==true && pressedKeys.s){
-        piglet.y += speed  
-    }
-    if( access==true &&pressedKeys.w){
-        piglet.y -= speed  
-        
-    }
-    if( access==true &&pressedKeys.a){
-        piglet.x -= speed  
-    }
-    if( access==true &&pressedKeys.d){
-        piglet.x += speed  
-    }
+                butcher.jump.up -= speed
+            }
+            if(pressedKeys.ArrowLeft){
+                butcher.x -=speed
+            }
+            if(pressedKeys.ArrowRight){
+                butcher.x +=speed
+            }
+            if( pressedKeys.s){
+                piglet.y += speed  
+            }
+            if(pressedKeys.w){
+                piglet.jump.up -= speed                  
+            }
+            // if(pressedKeys.a){
+            //     piglet.x -= 5  
+            // }
+            if(pressedKeys.d){
+                piglet.shiftRight() 
+            }
     
     access = true
 }
 
-document.addEventListener('keydown', function(e){
-    pressedKeys[e.key] = true
-    movement()
-    
-})
-document.addEventListener('keyup', function(e){
-    pressedKeys[e.key] = false
-    movement()
-})
 function defaultSetting(){
     ctx.fillStyle = "aquamarine"
-        ctx.fillRect(0,0,canvas.width,canvas.height)
-        leftWall.create()
-        topWall.create()
-        rightWall.create()
-        bottomWall.create()
-        // obstacle.create()
-    }
-    function game(){
-        defaultSetting()
-        butcher.gravityUpdate()
-        piglet.gravityUpdate()
-        console.log(access)
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    leftWall.create()
+    topWall.create()
+    rightWall.create()
+    bottomWall.create()
+    // obstacle.create()
+}
+function game(){
+    defaultSetting()
+    butcher.gravityUpdate()
+    piglet.gravityUpdate()
+    document.addEventListener('keydown', function(e){
+        pressedKeys[e.key] = true
+        movement()
         
-        obstacleBump(piglet,bottomWall)
+    })
+    document.addEventListener('keyup', function(e){
+        pressedKeys[e.key] = false
+        movement()
+    })
+        
+        //obstacleBump(piglet,bottomWall)
         //obstacleBump(butcher,bottomWall)
         
     }
-    // function onTop(obj1,obj2){
-
+    // function onTop(obj1){
+    //     const tSide = obj1.y + obj1.height <= canvas.height
     // }
     function obstacleBump(obj1,obj2){
         const bSide = obj2.y + obj2.height <= obj1.y
@@ -146,7 +163,6 @@ function defaultSetting(){
         //console.log(obj1.y + obj1.height<= obj2.y)
         if(bSide==false&&tSide==false&&lSide==false&&rSide==false){
            access = false
-           //console.log('game should end now')
         }
         else{
            access = true
@@ -154,10 +170,6 @@ function defaultSetting(){
     
     }
     
-    const refresh = setInterval( function(){
-        if(access==true){
-            game()
-        }
-    } , 80)
+    const refresh = setInterval(game, 10)
     
     
