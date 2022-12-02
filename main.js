500e87daca79d60b55125cbb26f5d2205cb43f78
robertoooc <robertooo.c48@gmail.com>
2022-12-01 20:53:30 -0800

    working on applying a form of gravity to allow jumps and falls

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ canvas.setAttribute('width',getComputedStyle(canvas)['width'])
 //getting rendering context from the Canvas
 const ctx = canvas.getContext('2d')
 
-//made a class that will help out with player movement
-class Players{
+class Objects{
     constructor(x,y,width,height,color){
         this.x =x
         this.y = y
@@ -16,19 +15,69 @@ class Players{
         this.height = height
         this.color = color
     }
+    create(){
+        ctx.fillStyle=this.color
+        ctx.fillRect(this.x,this.y,this.width,this.height)
+    } 
+    update(){
+
+    }
+}
+
+let leftWall = new Objects(0,0,1,canvas.height,"orange")
+let topWall = new Objects(0,0,canvas.width,1,"orange")
+let rightWall = new Objects(canvas.width -1,0,1,canvas.height,"orange")
+let bottomWall = new Objects(0,canvas.height-1,canvas.width,1,"orange")
+// let obstacle = new Objects(300,54,50,50,"black")
+// obstacle.create()
+// let line = new Objects()
+let access = true
+const downAccelerate = .5
+
+//made a class that will help out with player movement
+class Players{
+    constructor(x,y,color){
+        this.x = x
+        this.y = y
+        this.access
+        this.jump = {
+            up: 10,
+            gravity: 10
+        }
+        this.width = (canvas.height)/21
+        this.height = (canvas.width)/30
+        this.color = color
+    }
         create(){
             ctx.fillStyle=this.color
             ctx.fillRect(this.x,this.y,this.width,this.height)
         }
+        gravityUpdate(){
+            this.create()
+            this.y+=this.jump.gravity
+            console.log(access)
+
+            if(access ==false){
+                this.jump.gravity = 0
+            }
+            this.jump.y+=downAccelerate
+        }
+        // jumpUpdate(){
+        //     this.create()
+        //     this.y+=this.jump.up
+        // }
 
 }
 
-
-let piglet = new Players(5,10,45,45,'pink')
-piglet.create()
-let butcher = new Players(5,100,45,45,'red')
+let butcher = new Players(5,10,'red')
 butcher.create()
+ let piglet = new Players(40,90,'pink')
+piglet.create()
+
+
 let pressedKeys ={}
+
+
 
 const speed = 5;
 function movement(){
@@ -44,18 +93,21 @@ function movement(){
     if(pressedKeys.ArrowRight){
         butcher.x +=speed
     }
-    if(pressedKeys.s){
+    if( access==true && pressedKeys.s){
         piglet.y += speed  
     }
-    if(pressedKeys.w){
+    if( access==true &&pressedKeys.w){
         piglet.y -= speed  
+        
     }
-    if(pressedKeys.a){
+    if( access==true &&pressedKeys.a){
         piglet.x -= speed  
     }
-    if(pressedKeys.d){
+    if( access==true &&pressedKeys.d){
         piglet.x += speed  
     }
+    
+    access = true
 }
 
     document.addEventListener('keydown', function(e){
@@ -70,19 +122,35 @@ function movement(){
     function defaultSetting(){
         ctx.fillStyle = "aquamarine"
         ctx.fillRect(0,0,canvas.width,canvas.height)
-        let leftWall = new Players(0,0,2,canvas.height,"orange")
         leftWall.create()
-        let topWall = new Players(0,0,canvas.width,2,"orange")
         topWall.create()
-        let rightWall = new Players(canvas.width -2,0,2,canvas.height,"orange")
         rightWall.create()
-        let bottomWall = new Players(0,canvas.height-2,canvas.width,2,"orange")
         bottomWall.create()
+        // obstacle.create()
     }
     function game(){
         defaultSetting()
-        piglet.create()
-        butcher.create()
+        butcher.gravityUpdate()
+        piglet.gravityUpdate()
+    
+        obstacleBump(butcher,bottomWall)
+        obstacleBump(piglet,bottomWall)
+
+    }
+    function obstacleBump(obj1,obj2){
+         const bSide = obj2.y + obj2.height <= obj1.y
+         const lSide = obj2.x + obj2.width <= obj1.x
+         const rSide = obj1.x + obj1.width <= obj2.x 
+         const tSide = obj1.y + obj1.height <= obj2.y
+         if(bSide==false&&tSide==false&&lSide==false&&rSide==false){
+            access = false
+            console.log('game should end now')
+         }
+         else{
+            access = true
+         }
+
     }
     const refresh = setInterval(game, 50)
 
+ 
